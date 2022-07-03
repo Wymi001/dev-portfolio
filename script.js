ca93ea8dfd8fec13e04651994b8f9762ab4cea8b
Clement Adeyemi <adeyemiclement1@gmail.com>
2022-07-03 17:34:48 +0200

    my first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function openNav() {
+ document.getElementById('navigation').style.display = 'block';
+ document.getElementById('dropdown').style.display = 'none';
+ document.getElementById('closeIcon').style.display = 'block';
+}
