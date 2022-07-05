c4e31decf27d7b8d62a74409df8973fa0d48448a
Clement Adeyemi <adeyemiclement1@gmail.com>
2022-07-05 02:40:05 +0200

    some JS added

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function openNav() {
  document.getElementById('navigation').style.display = 'block';
- // document.getElementById('dropdown').style.display = 'none';
- document.getElementById('closeIcon').style.display = 'block';
+ document.getElementById('open-nav').style.display = 'none';
+ document.getElementById('close-nav').style.display = 'block';
+}
+function closeNav() {
+ document.getElementById('navigation').style.display = 'none';
+ document.getElementById('open-nav').style.display = 'block';
+ document.getElementById('close-nav').style.display = 'none';
 }
